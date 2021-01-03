46d8e82dab2684c96ef6785b94aa161ca96cc52b
Memet <meavci@gmail.com>
2021-01-03 15:29:54 -0500

    Redesing App.css file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './burger-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="Delicious Burger" id="logo" />
+      <hr></hr>
+
+      
+
+    </div>
+  );
+}
+
+export default App;
