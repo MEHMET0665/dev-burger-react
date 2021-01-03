406f20793a21caf4025894cacd4f1c0de021300b
Memet <meavci@gmail.com>
2021-01-03 17:40:20 -0500

    Add login functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
+import React from 'react';
+import Login from './components/Login'
 import logo from './burger-logo.png';
 import './App.css';
-import Login from './components/Login'
-import React from'react'
 
 class App extends React.Component {
-  render(){
-  return (
-    <div className="App">
-      <img src={logo} alt="Delicious Burger" id="logo" />
-      <hr></hr>
-
-      
-      <Login/>
-    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      username:'',
+      password:'',
+      isLogin:false
+    };
+    this.usercheck = this.usercheck.bind(this);
+    this.setUserName=this.setUserName.bind(this)
+    this.setPassword=this.setPassword.bind(this)
+  }
   
-  );}
+  usercheck(){
+    if (this.state.username === "abc" && this.state.password === "123" ){
+      this.setState({
+        isLogin:true
+      })
+    } else {
+      alert('Username and Password doesn\'t match');
+    }
+  }
+
+  setUserName(UserName){
+    this.setState({
+     username:UserName
+    })
+  }
+
+  setPassword(UserPassword){
+    this.setState({
+     password:UserPassword
+    })
+  }
+
+  render(){
+    let currComponent=<div></div>
+    if (this.state.isLogin){
+      currComponent=<h1>You are successfully Logged in</h1>
+
+    }else{
+      currComponent=<Login userName={this.setUserName}
+      userpassword={this.setPassword}checkUser={this.usercheck}/>
+    }
+    return (
+      <div className="App">
+        <img src={logo} alt="Delicious Burger" id="logo" />
+        <hr></hr>
+        {currComponent}
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
